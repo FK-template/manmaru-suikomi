a3033fbad3b62d9025c05e8c5ccb28ff99637a5d
shikaTUT <m0a232893f@edu.teu.ac.jp>
2026-03-21 01:06:18 +0900

    【追加】エリアごとのカメラ切り替え
    ※注意点：Playerに「UseGravityオフ」「IsKinematicオン」のRigidbodyをアタッチしました。念のため、PhysicsSettingsで衝突レイヤー設定も変更しています。

---
+++ Assets/01_Scripts/System/CameraChangeTrigger.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using Unity.Cinemachine;
+
+namespace Manmaru.System
+{
+    public class CameraChangeTrigger : MonoBehaviour
+    {
+        [Header("この区間で起動するカメラ")]
+        [SerializeField] private CinemachineCamera _targetCamera;
+
+        [Header("カメラ遷移判定を取るレイヤー")]
+        [SerializeField] private LayerMask _playerLayer;
+
+        [Header("起動時の優先度")]
+        [SerializeField] private int _activePriority = 100;
+
+        // 内部変数：元々の優先度
+        private int _defaultPriority;
+
+        void Start()
+        {
+            _defaultPriority = _targetCamera.Priority;
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            Debug.Log("Enter");
+            
+            if (((1 << other.gameObject.layer) & _playerLayer) != 0)
+            {
+                ChangeCameraPriority(_activePriority);
+                Debug.Log("Player Enter");
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (((1 << other.gameObject.layer) & _playerLayer) != 0)
+                ChangeCameraPriority(_defaultPriority);
+        }
+
+        /// <summary>
+        /// 担当カメラの優先度を変更するメソッド
+        /// </summary>
+        private void ChangeCameraPriority(int targetPriority)
+        {
+            _targetCamera.Priority = targetPriority;
+        }
+    }
+}
