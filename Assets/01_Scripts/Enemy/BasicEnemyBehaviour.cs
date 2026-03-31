e0659ca1fcffeb9cb732fad38e7bbc05ded21891
shikaTUT <m0a232893f@edu.teu.ac.jp>
2026-04-01 04:07:00 +0900

    【変更】標準敵向けの状態クラス作成処理と遷移関係の設定を追加

--- Assets/01_Scripts/Enemy/BasicEnemyBehaviour.cs
+++ Assets/01_Scripts/Enemy/BasicEnemyBehaviour.cs
@@ -10,6 +10,9 @@ namespace Manmaru.Enemy
     {
         // 内部変数：状態クラス
         private PatrolStateLogic _patrol;
+        private NoticeStateLogic _notice;
+        private DashStateLogic _dash;
+        private CooldownStateLogic _kyoro;
 
         protected override void Start()
         {
@@ -17,9 +20,15 @@ namespace Manmaru.Enemy
 
             // 状態クラス生成
             _patrol = new PatrolStateLogic(this);
+            _notice = new NoticeStateLogic(this);
+            _dash = new DashStateLogic(this);
+            _kyoro = new CooldownStateLogic(this);
 
             // 状態遷移設定
-            _patrol.OnPlayerFound += () => Debug.Log("プレイヤー発見！とっしん状態に移行予定");
+            _patrol.OnPlayerFound += () => ChangeState(_notice);
+            _notice.OnChargeReady += () => ChangeState(_dash);
+            _dash.OnDashFinished += () => ChangeState(_kyoro);
+            _kyoro.OnCooldownFinished += () => ChangeState(_patrol);
 
             ChangeState(_patrol);
         }
