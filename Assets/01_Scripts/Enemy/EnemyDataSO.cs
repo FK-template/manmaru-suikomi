a00e83ccfbeca8076888329a5ddeb50b965e48ac
shikaTUT <m0a232893f@edu.teu.ac.jp>
2026-03-31 17:29:28 +0900

    【変更】データ定義に当たり判定のサイズ等を追加

--- Assets/01_Scripts/Enemy/EnemyDataSO.cs
+++ Assets/01_Scripts/Enemy/EnemyDataSO.cs
@@ -11,16 +11,22 @@ namespace Manmaru.Enemy
         [Header("基本ステータス")]
         public float MaxHP = 1.0f;
 
+        [Header("当たり判定ステータス")]
+        public float BodyRadius = 0.5f;
+
+        [Header("基本移動ステータス")]
+        public float RotationSpeed = 360.0f;
+        public float ActionWaitSecond = 1.0f;
+
         [Header("うろうろ移動ステータス")]
         public float PatrolSpeed = 1.0f;
         public float PatrolRadius = 5.0f;
         public float ReachThreshold = 1.0f;
-        
-        public float DashSpeed = 3.0f;
-        public float ActionWaitSecond = 1.0f;
-        public float RotationSpeed = 360.0f;
 
-        [Header("視界センサー（干渉判定）系")]
+        [Header("とっしん移動ステータス")]
+        public float DashSpeed = 3.0f;
+
+        [Header("視界センサーステータス")]
         public bool CanDetectPlayer = true;
         public float SightRange = 3.0f;
         public float SightAngle = 30.0f;
