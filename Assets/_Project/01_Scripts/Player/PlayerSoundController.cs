0dfff85456831567ffe363cd1827efcc80b42768
shikaTUT <m0a232893f@edu.teu.ac.jp>
2026-04-06 12:24:23 +0900

    【追加】プレイヤーのサウンドを制御するクラスを作成し、プレイヤーに必要コンポーネントを設定

---
+++ Assets/_Project/01_Scripts/Player/PlayerSoundController.cs
@@ -0,0 +1,26 @@
+using Manmaru.Audio;
+using Manmaru.Collision;
+using Manmaru.Movement;
+using UnityEngine;
+
+namespace Manmaru.Player
+{
+    /// <summary>
+    /// プレイヤーのサウンド処理全般を制御するクラス
+    /// </summary>
+    public class PlayerSoundController : MonoBehaviour
+    {
+        [Header("スピーカー設定")]
+        [SerializeField] private AudioSource _source;
+
+        void Start()
+        {
+
+        }
+
+        void Update()
+        {
+
+        }
+    }
+}
