2ec540e12dff94cdd11563add856c2c4e4ccf492
shikaTUT <m0a232893f@edu.teu.ac.jp>
2026-04-08 13:24:06 +0900

    【追加】自動でエフェクトを再生し、再生が完了したら自身を破棄するクラスを作成

---
+++ Assets/_Project/01_Scripts/VFX/OneShotEffectHandler.cs
@@ -0,0 +1,100 @@
+using Manmaru.Audio;
+using System.Collections;
+using UnityEngine;
+
+namespace Manmaru.VFX
+{
+    public class OneShotEffectHandler : MonoBehaviour
+    {
+        [Header("ビジュアルエフェクト設定")]
+        [SerializeField] private ParticleSystem[] _particles;
+
+        [Header("サウンドエフェクト設定")]
+        [SerializeField] private AudioEventSO[] _sounds;
+        [SerializeField] private AudioSource _source;
+
+        [Header("状況確認用パラメータ設定")]
+        [SerializeField] private float _checkIntervalSecond = 0.1f;
+
+        void Start()
+        {
+            PlayAllEffects();
+            StartCoroutine(CheckAndDestroyEffectsRoutine());
+        }
+
+        /// <summary>
+        /// 設定されたビジュアルエフェクトとサウンドエフェクトを全て単発再生するメソッド
+        /// </summary>
+        private void PlayAllEffects()
+        {
+            PlayAllVisualEffects();
+            PlayAllSoundEffects();
+        }
+
+        /// <summary>
+        /// 設定されたビジュアルエフェクトを全て単発再生するメソッド
+        /// </summary>
+        private void PlayAllVisualEffects()
+        {
+            if (_particles == null || _particles.Length == 0) return;
+            foreach (var p in _particles)
+            {
+                if (p == null || !p.isPlaying) return;
+                p.Play();
+            }
+        }
+
+        /// <summary>
+        /// 設定されたサウンドエフェクトを全て単発再生するメソッド
+        /// </summary>
+        private void PlayAllSoundEffects()
+        {
+            if (_sounds == null || _sounds.Length == 0) return;
+            foreach (var s in _sounds)
+            {
+                if (s == null) return;
+                s.PlayRandomPitch(_source);
+            }
+        }
+
+        /// <summary>
+        /// 全てのエフェクトの終了を監視し、完了後に自身を破棄するコルーチン
+        /// </summary>
+        /// <remarks>（パーティクルは粒が全て消えたら完了にしたいので、isPlayingではなくIsAlive(true)で状況チェック）</remarks>
+        private IEnumerator CheckAndDestroyEffectsRoutine()
+        {
+            while (true)
+            {
+                bool isWorking = false;
+
+                // サウンド再生状況チェック
+                if (_source.isPlaying)
+                {
+                    isWorking = true;
+                }
+
+                // パーティクル再生状況チェック
+                foreach (var p in _particles)
+                {
+                    if (p != null && p.IsAlive(true))
+                    {
+                        isWorking = true;
+                        break;
+                    }
+                }
+
+                // 全ての再生が終わっていたら、ループ脱出
+                if (!isWorking)
+                {
+                    break;
+                }
+
+                // ちょっと待ってからまたチェックする（負荷対策）
+                yield return new WaitForSeconds(_checkIntervalSecond);
+            }
+
+            // 全て再生が終わったら、自身を破棄
+            Destroy(gameObject);
+        }
+    }
+}
