8cdfb90b81ce6c2502b3cd95e3b84053fab3eb3a
shikaTUT <m0a232893f@edu.teu.ac.jp>
2026-04-08 14:49:09 +0900

    【変更】プレイヤーのエフェクト生成専門クラスを作成

---
+++ Assets/_Project/01_Scripts/Player/PlayerEffectSpawner.cs
@@ -0,0 +1,39 @@
+using Manmaru.Effect;
+using UnityEngine;
+
+namespace Manmaru.Player
+{
+    /// <summary>
+    /// プレイヤーのエフェクト生成を統括するクラス
+    /// </summary>
+    public class PlayerEffectSpawner : MonoBehaviour
+    {
+        [Header("エフェクト設定")]
+        [SerializeField] private OneShotEffectHandler _damagedEffect;
+
+        [Header("依存クラス設定")]
+        [SerializeField] private PlayerHealthController _healthController;
+
+        void Start()
+        {
+            // イベント購読設定
+            _healthController.OnTookDamage += SpawnDamagedEffect;
+        }
+
+        /// <summary>
+        /// 被ダメージ時のエフェクトを生成するメソッド
+        /// </summary>
+        private void SpawnDamagedEffect()
+        {
+            Instantiate(_damagedEffect, transform.position, Quaternion.identity);
+        }
+        private void OnDestroy()
+        {
+            // イベント購読解除
+            if (_healthController != null)
+            {
+                _healthController.OnTookDamage -= SpawnDamagedEffect;
+            }
+        }
+    }
+}
