9a382c76b529eb5df732ab6254817b6293fbd687
shikaTUT <m0a232893f@edu.teu.ac.jp>
2026-05-20 12:57:46 +0900

    【サウンド/追加】BGMPlayerプレハブをタイトルに配置し、TitleScenePresenterから再生できるように変更・アセット作成

--- Assets/_Project/01_Scripts/UI/TitleScenePresenter.cs
+++ Assets/_Project/01_Scripts/UI/TitleScenePresenter.cs
@@ -1,3 +1,4 @@
+using Manmaru.Audio;
 using Manmaru.System;
 using UnityEngine;
 
@@ -8,6 +9,9 @@ namespace Manmaru.UI
     /// </summary>
     public class TitleScenePresenter : MonoBehaviour
     {
+        [Header("シーン開幕時パラメータ設定")]
+        [SerializeField] private BGMDataSO _titleStartBGMData;
+
         [Header("タイトルシーンで管理する画面")]
         [SerializeField] private TitleScreen _titleScreen;
         [SerializeField] private StageSelectScreen _stageSelectScreen;
@@ -16,12 +20,26 @@ namespace Manmaru.UI
         [Header("ゲーム終了システム")]
         [SerializeField] private AppQuitHandler _appQuitHandler;
 
+        // 内部変数
+        private BGMPlayer _bgmPlayer;
+
         void Start()
         {
+            _bgmPlayer = BGMPlayer.Instance;
+
             // 各画面のボタンの役割設定
             _titleScreen.OnStartClicked += MoveToStageSelect;
             _stageSelectScreen.OnReturnClicked += MoveToTitle;
             _titleScreen.OnQuitClicked += () => _confirmScreen.ShowDialog(_appQuitHandler.QuitGame);
+            PresentTitleStart();
+        }
+
+        /// <summary>
+        /// タイトル演出を再生するメソッド
+        /// </summary>
+        private void PresentTitleStart()
+        {
+            _bgmPlayer.PlayJingleAndBGM(_titleStartBGMData);
         }
 
         /// <summary>
